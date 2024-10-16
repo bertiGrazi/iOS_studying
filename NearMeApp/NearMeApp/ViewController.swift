f5ca03ede8784c48977b23cdeda4a5621b089686
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2024-10-16 18:40:59 -0300

    Adding Search TextField on the Screen

--- NearMeApp/NearMeApp/ViewController.swift
+++ NearMeApp/NearMeApp/ViewController.swift
@@ -16,6 +16,18 @@ class ViewController: UIViewController {
         mapView.translatesAutoresizingMaskIntoConstraints = false
         return mapView
     }()
+    
+    lazy var searchTextField: UITextField = {
+        let searchTextField = UITextField()
+        searchTextField.placeholder = "Search for a location"
+        searchTextField.layer.cornerRadius = 10
+        searchTextField.clipsToBounds = true
+        searchTextField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 0))
+        searchTextField.leftViewMode = .always
+        searchTextField.backgroundColor = .white
+        searchTextField.translatesAutoresizingMaskIntoConstraints = false
+        return searchTextField
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,12 +35,22 @@ class ViewController: UIViewController {
     }
     
     private func setupUI() {
+        view.addSubview(searchTextField)
         view.addSubview(mapView)
+        
+        view.bringSubviewToFront(searchTextField)
+        
+        searchTextField.heightAnchor.constraint(equalToConstant: 44).isActive = true
+        searchTextField.widthAnchor.constraint(equalToConstant: view.bounds.size.width/1.2).isActive = true
+        searchTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        searchTextField.topAnchor.constraint(equalTo: view.topAnchor, constant: 60).isActive = true
+        searchTextField.returnKeyType = .go
         
         mapView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
         mapView.heightAnchor.constraint(equalTo: view.heightAnchor).isActive = true
         mapView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         mapView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+       
     }
 }
 
