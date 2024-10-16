dbb1a49ab2b83a1e0de2f414cc5579c99cf62a23
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2024-10-16 18:30:27 -0300

    Adding Map on the Screen

--- NearMeApp/NearMeApp/ViewController.swift
+++ NearMeApp/NearMeApp/ViewController.swift
@@ -6,12 +6,29 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
+    
+    lazy var mapView: MKMapView = {
+        let mapView = MKMapView()
+        mapView.showsUserLocation = true
+        mapView.translatesAutoresizingMaskIntoConstraints = false
+        return mapView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .magenta
+        setupUI()
+    }
+    
+    private func setupUI() {
+        view.addSubview(mapView)
+        
+        mapView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        mapView.heightAnchor.constraint(equalTo: view.heightAnchor).isActive = true
+        mapView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        mapView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
     }
 }
 
