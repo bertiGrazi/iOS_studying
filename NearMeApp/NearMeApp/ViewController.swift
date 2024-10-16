a45e2ed192ceb063cfab0444917cde22f36d56c1
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2024-10-16 18:20:34 -0300

    Create App

---
+++ NearMeApp/NearMeApp/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  NearMeApp
+//
+//  Created by Grazi  Berti on 16/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .magenta
+    }
+}
+
